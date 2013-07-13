e3e9245e286ca9329af4aa543538c9ffeb31cd5a
nithinr6 <nithinr6@outlook.com>
2013-07-14 01:07:46 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,17 +1,14 @@
-var express = require('express');
+ var express = require('express');
 
-var fs= require('fs');
-
-var app = express.createServer(express.logger());
-
-var index =fs.readFileSync("index.html");
+var fs =  require('fs');
+ var app = express.createServer(express.logger());
+ 
+var index = fs.readFileSync("index.html");
 var buffer = new Buffer(index);
-app.get('/', function(request, response) {
+
+ app.get('/', function(request, response) {
   response.send('Hello World! 2');
   response.send(buffer.toString());
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+ });
+ 
+ var port = process.env.PORT || 5000;
