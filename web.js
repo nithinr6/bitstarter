8f19faae4420b0085c96c4ea351d2edf8c1ffb04
nithinr6 <nithinr6@outlook.com>
2013-07-16 11:21:39 +0530

    Update web.js
    display index.html

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
- var express = require('express');
+var express = require('express');
+var fs = require('fs');
+var content = new Buffer (fs.readFileSync("index.html"), "utf-8");
 
-var fs =  require('fs');
- var app = express.createServer(express.logger());
- 
-var index = fs.readFileSync("index.html");
-var buffer = new Buffer(index);
+var app = express.createServer(express.logger());
 
- app.get('/', function(request, response) {
-  response.send('Hello World! 2');
-  response.send(buffer.toString());
- });
- 
- var port = process.env.PORT || 5000;
+app.get('/', function(request, response) {
+  response.send(content.toString("utf-8",0,999999999999));
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
